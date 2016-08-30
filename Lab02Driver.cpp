b4546fc162e99d1c9fc440625ea8f5c2467e4d5d
Landon Craig <lscraig42@students.tntech.edu>
2016-08-30 14:22:46 -0500

    Added the Lab02Files.zip

---
+++ Lab02Driver.cpp
@@ -0,0 +1,26 @@
+//test comment
+#include "ReadFile.h"
+#include "WriteFile.h"
+#include "String.h"
+#include "Tokens.h"
+
+#include <iostream>
+int main()
+{
+   ReadFile* rf = new ReadFile("cds.txt");
+   WriteFile* wf = new WriteFile("out.txt");
+
+   while(!rf->eof())
+   {
+      String* line = rf->readLine();
+      wf->writeLine(line);
+      delete line;
+   }
+
+   rf->close();
+   wf->close();
+   delete rf;
+   delete wf;
+
+   return 0;
+}
